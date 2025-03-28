b01ce3c2561681653f2fedd11b048fdd1e0bb702
Edoardo Baldi <edoardob90@gmail.com>
2025-03-28 13:52:28 +0100

    Prepare deployment on GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://empa-scientific-it.github.io",
+  base: "/docs",
   integrations: [
     starlight({
       title: "Empa Scientific IT",

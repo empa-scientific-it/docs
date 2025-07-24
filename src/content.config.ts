f6082052c4afb303428fdfd5c35514108792db4d
Edoardo Baldi <edoardob90@gmail.com>
2025-07-24 16:49:53 +0200

    Refactor labs documentation structure
    * Added labId to lab markdown files for better identification
    * Implemented dynamic lab selection with dropdown in the LabSelector
    * Created new pages for labs 403 and 404

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,15 +1,19 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
-const labsCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
+const docs = defineCollection({ loader: docsLoader(), schema: docsSchema() });
+const labs = defineCollection({
+  loader: glob({
+    pattern: "**/[^_]*.(md|mdx)",
+    base: "./src/content/labs",
+  }),
+  schema: docsSchema({
+    extend: z.object({
+      labId: z.number(),
+    }),
   }),
 });
 
-export const collections = {
-  docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-  labs: labsCollection,
-};
+export const collections = { docs, labs};
